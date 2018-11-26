0727c1d9149b2003f3926e64dc54081eeb12f3fc
DavidMutchler <mutchler@rose-hulman.edu>
2018-11-26 10:25:09 -0500

    done

--- src/m2_hello_world.py
+++ src/m2_hello_world.py
@@ -17,7 +17,7 @@ print('Hello, World')
 
 ###############################################################################
 #
-# TODO: 1.
+# DONE: 1.
 #   (Yes, that means for YOU to DO things per these instructions:)
 #
 #   Run this module by right clicking anywhere in this window and selecting
@@ -32,7 +32,7 @@ print('Hello, World')
 
 ###############################################################################
 #
-# TODO: 2.
+# DONE: 2.
 #   Notice the small horizontal BLUE bars on the scrollbar-like thing
 #   on the right.  Each blue bar indicates a thing TO-DO in this module.
 #
@@ -67,6 +67,7 @@ print('Hello, World')
 #   Look at the Run window to be sure that your string printed as expected.
 #
 ###############################################################################
+print('this is cool!')
 
 ###############################################################################
 #
@@ -81,6 +82,7 @@ print('Hello, World')
 #   (HINT: It is an INTERESTING number.)  Get help if your value is wrong.
 #
 ###############################################################################
+print(3607 * 34227)
 
 ###############################################################################
 #
